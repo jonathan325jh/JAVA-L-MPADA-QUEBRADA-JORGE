579a2962b1a4264d317c2f14933f17662f735dbe
Jonathan Erick <jonathan.santos.dias@escola.pr.gov.br>
2024-11-19 15:47:43 -0300

    Update lamp.js

--- lamp.js
+++ lamp.js
@@ -2,15 +2,28 @@ var $ligar = document.getElementById('ligar')
 var $desligar = document.getElementById('desligar')
 var $lampada = document.getElementById('lamp')
 
-function ligarLamp(){
-      $lampada.src ='./lampada-acesa.jpg'
+function lampQuebrada(){
+          return $lampada.src.indexOf('quebrada')>-1
+}
+
+function ligarLamp(){  
+   if(!lampQuebrada()) {
+              $lampada.src ='./lampada-acesa.jpg'
+        }
 }
 
 function desligarLamp(){
     $lampada.src ='./lampada-apagada.jpg'
 }
 
+function quebrarLamp(){
+    if(!lampQuebrada()) {
+           $lampada.src ='./lampada-quebrada.jpg'
+       }
+}
+
 
 $ligar.addEventListener('click',ligarLamp)
-
 $desligar.addEventListener('click',desligarLamp)
+$lampada.addEventListener('mouseleave',desligarLamp)
+$lampada,addEventListener('dblclick',quebrarLamp)

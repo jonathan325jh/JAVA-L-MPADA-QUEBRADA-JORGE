e6d19e11dc8ac41911a05b2eb6124afb34bdeea2
Jonathan Erick <jonathan.santos.dias@escola.pr.gov.br>
2024-11-26 13:45:05 -0300

    Update lamp.js

--- lamp.js
+++ lamp.js
@@ -1,30 +1,32 @@
-var $ligar = document.getElementById('ligar')
-var $desligar = document.getElementById('desligar')
-var $lampada = document.getElementById('lamp')
+var ligar = document.getElementById('ligar')
+var desligar = document.getElementById('desligar')
+var lampada = document.getElementById('lamp')
 
 function lampQuebrada(){
-          return $lampada.src.indexOf('quebrada')>-1
+          return lampada.src.indexOf('quebrada')>-1
 }
 
 function ligarLamp(){  
    if(!lampQuebrada()) {
-              $lampada.src ='./lampada-acesa.jpg'
+              lampada.src ='./lampada-acesa.jpg'
         }
 }
 
 function desligarLamp(){
-    $lampada.src ='./lampada-apagada.jpg'
+    if(!lampQuebrada()) {
+            lampada.src ='./lampada-apagada.jpg'
+       }
+
 }
 
 function quebrarLamp(){
-    if(!lampQuebrada()) {
-           $lampada.src ='./lampada-quebrada.jpg'
+           lampada.src ='./lampada-quebrada.jpg'
        }
-}
 
 
-$ligar.addEventListener('click',ligarLamp)
-$desligar.addEventListener('click',desligarLamp)
-$lampada.addEventListener('mouseover',ligarLamp)
-$lampada.addEventListener('mouseleave',desligarLamp)
-$lampada,addEventListener('dblclick',quebrarLamp)
+
+ligar.addEventListener('click',ligarLamp)
+desligar.addEventListener('click',desligarLamp)
+lampada.addEventListener('mouseover',ligarLamp)
+lampada.addEventListener('mouseleave',desligarLamp)
+lampada,addEventListener('dblclick',quebrarLamp)
